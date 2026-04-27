d24dfe3e2faa877ee18cf747bf041f69cb589124
Gabriel Cássio <gabriel.cgc.contato@gmail.com>
2026-04-27 08:48:46 -0300

    feat(mempool): Implemented basic mempool

--- core/src/utils/decode_signature.cpp
+++ core/src/utils/decode_signature.cpp
@@ -6,11 +6,9 @@
 std::vector<unsigned char> decode_signature(const std::string& signature) {
     std::vector<unsigned char> bytes;
 
-
     if (signature.length() % 2 != 0) {
         throw std::invalid_argument("Tamanho de assinatura hexadecimal inválido.");
     }
-
 
     for (size_t i = 0; i < signature.length(); i += 2) {
 
